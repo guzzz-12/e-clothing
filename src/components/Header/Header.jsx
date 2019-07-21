06033c4bcb4ed2e6f65753f3dcafcf3d004913e4
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-21 13:01:07 -0400

    Firebase: Se muestra botón sign in/sign out en el Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import "./header.scss";
 import {Link} from "react-router-dom";
 import {ReactComponent as HeaderLogo} from "../../assets/crown.svg";
+import {auth} from "../../firebase/fiebaseUtils";
 
-const Header = () => {
+const Header = (props) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -12,6 +13,11 @@ const Header = () => {
       <div className="options">
         <Link className="option" to="/shop">Shop</Link>
         <Link className="option" to="/contact">Contact</Link>
+        {props.currentUser ?
+          <div className="option" onClick={() => auth.signOut()}>Sign out</div>
+          :
+          <Link className="option" to="/signin">Sign in</Link>
+        }
       </div>
     </div>
   );
