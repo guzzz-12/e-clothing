4e1294279727f7c4b5049c9dd1c3d1538af452fe
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-21 07:50:17 -0400

    Agregado header al root de la aplicación

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './app.css';
 import HomePage from "./pages/HomePage/HomePage";
 import {Route, Switch} from "react-router-dom";
 import Shop from './pages/Shop/Shop';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route exact path="/shop" component={Shop}/>
