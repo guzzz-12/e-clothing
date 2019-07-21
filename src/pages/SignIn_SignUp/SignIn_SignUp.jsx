d2164d73e6dfbebc5debb14615fa4975d0398351
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-21 16:59:14 -0400

    Componente SignUp agregado a la página SignIn_SignUp

--- src/pages/SignIn_SignUp/SignIn_SignUp.jsx
+++ src/pages/SignIn_SignUp/SignIn_SignUp.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import "./signIn_SignUp.scss";
 import SignIn from '../../components/SignIn/SignIn';
+import SignUp from '../../components/SignUp/SignUp';
 
 const SignIn_SignUp = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
 
   );
