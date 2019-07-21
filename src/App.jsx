85401b2009e41d194dca4a5329245f8eac8725b9
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-20 22:58:29 -0400

    Ruta al componente Shop

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './app.css';
 import HomePage from "./pages/HomePage/HomePage";
 import {Route, Switch} from "react-router-dom";
+import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage}/>
+        <Route exact path="/shop" component={Shop}/>
       </Switch>
     </div>
   );
