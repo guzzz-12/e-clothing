937392303106598ae70b4619eb2fc945e5233687
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-21 08:22:31 -0400

    Página de sign in

---
+++ src/pages/SignIn_SignUp/SignIn_SignUp.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import SignIn from '../../components/SignIn/SignIn';
+
+const SignIn_SignUp = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+    </div>
+
+  );
+};
+
+export default SignIn_SignUp;
