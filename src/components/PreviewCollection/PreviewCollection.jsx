b4c706e7972790602eb3ff95df94dc4f9278a15c
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-20 23:45:20 -0400

    ColectionItem agregado al componente PreviewCollection

--- src/components/PreviewCollection/PreviewCollection.jsx
+++ src/components/PreviewCollection/PreviewCollection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./previewCollection.scss";
+import CollectionItem from '../CollectionItem/CollectionItem';
 
 const PreviewCollection = (props) => {
   return (
@@ -7,7 +8,14 @@ const PreviewCollection = (props) => {
       <h1 className="title">{props.title.toUpperCase()}</h1>
       <div className="preview">
         {props.items.map(item => {
-          return <div key={item.id}>{item.name}</div>
+          return (
+            <CollectionItem
+              key={item.id}
+              imageUrl={item.imageUrl}
+              name={item.name}
+              price={item.price}
+            />
+          )
         }).slice(0, 4)}
       </div>
     </div>
