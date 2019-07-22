3a3c90bef90ee9ca85ad0a652e6da009a831c2ee
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-22 15:10:49 -0400

    Redux: Action y reducer para agregar los items al store

--- src/redux/cart/cartReducer.js
+++ src/redux/cart/cartReducer.js
@@ -1,13 +1,19 @@
 import {cartActionTypes} from "./cartTypes";
 
 const initialState = {
-  showDropdown: false
+  showDropdown: false,
+  cartItems: []
 }
 
 const cartReducer = (state = initialState, action) => {
   switch(action.type) {
     case cartActionTypes.SHOW_HIDE_DROPDOWN:
       return {...state, showDropdown: !state.showDropdown}
+    case cartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      }
     default:
       return state;
   }
