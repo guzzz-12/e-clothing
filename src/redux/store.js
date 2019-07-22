c1c6b7d664a7afbb30f889a394fabbb0d4fd5c53
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-22 09:03:38 -0400

    Redux: Agregado redux devtools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
 import {createStore, applyMiddleware} from "redux";
 import logger from "redux-logger";
 import rootReducer from "./rootReducer";
+import {composeWithDevTools} from "redux-devtools-extension";
 
 const middlewares = [logger];
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const composedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
+const store = createStore(rootReducer, composedEnhancer);
+// const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export default store;
