55c5452ed7dd25bb029c51ee260dd49817ee9c67
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-22 09:30:32 -0400

    Redux: Se toma el current user del store

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,6 +3,7 @@ import "./header.scss";
 import {Link} from "react-router-dom";
 import {ReactComponent as HeaderLogo} from "../../assets/crown.svg";
 import {auth} from "../../firebase/firebaseUtils";
+import {connect} from "react-redux";
 
 const Header = (props) => {
   return (
@@ -23,4 +24,10 @@ const Header = (props) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    currentUser: state.user.currentUser
+  }
+}
+
+export default connect(mapStateToProps)(Header);
