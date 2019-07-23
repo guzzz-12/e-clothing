9b959fe9dbed67e5f7cf458abebb1ce2b11bd26a
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-23 09:04:55 -0400

    Componente Cart-Item: Muestra los items agregados al cart en el dropdown

---
+++ src/components/Cart-Item/Cart-Item.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./cart-Item.scss";
+
+const CartItem = (props) => {
+  return (
+    <div className="cart-item">
+      <img src={props.item.imageUrl} alt="item" />
+      <div className="item-details">
+        <span className="name">{props.item.name}</span>
+        <span className="price">{props.item.quantity} x {props.item.price}</span>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
