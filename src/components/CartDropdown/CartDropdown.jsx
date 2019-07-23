cb443320900322ac892601ac3fff2de8fb2a6a10
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-23 09:06:11 -0400

    Redux: Se genera un Cart-Item por cada item en el store

--- src/components/CartDropdown/CartDropdown.jsx
+++ src/components/CartDropdown/CartDropdown.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import "./cartDropdown.scss";
 import CustomBtn from "../CustomButton/CustomBtn";
+import {connect} from "react-redux";
+import CartItem from '../Cart-Item/Cart-Item';
 
-const CartDropdown = () => {
+const CartDropdown = (props) => {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {props.items.map(item => {
+          return <CartItem key={item.id} item={item} />
+        })}
+      </div>
       <CustomBtn>Go to checkout</CustomBtn>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToProps = (state) => {
+  return {
+    items: state.cartDropdown.cartItems
+  }
+}
+
+export default connect(mapStateToProps)(CartDropdown);
