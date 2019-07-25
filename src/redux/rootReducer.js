85594a932f8732508de96fd26fdd66fc8c98094d
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-25 08:10:00 -0400

    Redux: Agregado redux-persist para persistencia de datos

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,7 +2,23 @@ import {combineReducers} from "redux";
 import userReducer from "./user/userReducer";
 import cartReducer from "./cart/cartReducer";
 
-export default combineReducers({
+import {persistReducer} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cartDropdown"]
+}
+
+const rootReducer = combineReducers({
   user: userReducer,
   cartDropdown: cartReducer
 })
+
+export default persistReducer(persistConfig, rootReducer)
+
+// export default combineReducers({
+//   user: userReducer,
+//   cartDropdown: cartReducer
+// })
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
+import {persistStore} from "redux-persist";
 
 const middlewares = [];
 const composedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
-const store = createStore(rootReducer, composedEnhancer);
-// const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, composedEnhancer);
+export const persistor = persistStore(store);
 
-export default store;
+export default {store, persistor};
