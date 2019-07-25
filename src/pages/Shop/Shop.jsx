9d4b0725440e5a2e44c15a985ae74a0dde9bc091
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-25 10:42:00 -0400

    Refactorizada página Shop

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,30 +1,12 @@
 import React from 'react';
-import PreviewCollection from '../../components/PreviewCollection/PreviewCollection';
-import {connect} from "react-redux";
+import CollectionsOverview from "../../components/CollectionsOverview/CollectionsOverview";
 
-const Shop = (props) => {
+const Shop = () => {
   return (
     <div className="shop-page">
-      {props.collections.map(collection => {
-        return (
-          <PreviewCollection
-            key={collection.id}
-            title={collection.title}
-            items={collection.items}
-          />
-        )
-      })}
+      <CollectionsOverview />
     </div>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    collections: state.shopData
-  }
-}
-
-export default connect(mapStateToProps)(Shop);
-
-
-
+export default Shop;
