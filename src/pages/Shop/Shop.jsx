6c2b2687cb57d08067e7c392b1cf7fc137e21c0c
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-25 08:57:17 -0400

    Refactorizada página Shop: La data se toma del store

--- src/pages/Shop/Shop.jsx
+++ src/pages/Shop/Shop.jsx
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import {shopData} from "./shopData";
+import React from 'react';
 import PreviewCollection from '../../components/PreviewCollection/PreviewCollection';
+import {connect} from "react-redux";
 
-class Shop extends Component {
-  state = {
-    collections: shopData
-  }
+const Shop = (props) => {
+  return (
+    <div className="shop-page">
+      {props.collections.map(collection => {
+        return (
+          <PreviewCollection
+            key={collection.id}
+            title={collection.title}
+            items={collection.items}
+          />
+        )
+      })}
+    </div>
+  );
+}
 
-  render() {
-    return (
-      <div className="shop-page">
-        {this.state.collections.map(collection => {
-          return (
-            <PreviewCollection
-              key={collection.id}
-              title={collection.title}
-              items={collection.items}
-            />
-          )
-        })}
-      </div>
-    );
+const mapStateToProps = (state) => {
+  return {
+    collections: state.shopData
   }
 }
 
-export default Shop;
+export default connect(mapStateToProps)(Shop);
 
 
 
