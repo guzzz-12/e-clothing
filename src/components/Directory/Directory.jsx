9f210e09f241e20fd6b1e4403efdcc4813bc8fc5
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-25 08:37:23 -0400

    Refactorizado el directorio, las categorías se toman del store

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,54 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import "./directory.scss";
 import MenuItem from '../MenuItem/MenuItem';
+import {connect} from "react-redux";
 
-class Directory extends Component {
-  state = {
-    sections: [
-      {
-        title: 'hats',
-        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-        id: 1,
-        linkUrl:"hats"
-      },
-      {
-        title: 'jackets',
-        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-        id: 2,
-        linkUrl:""
-      },
-      {
-        title: 'sneakers',
-        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-        id: 3,
-        linkUrl:""
-      },
-      {
-        title: 'women',
-        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-        size: "large",
-        id: 4,
-        linkUrl:""
-      },
-      {
-        title: 'men',
-        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-        size: "large",
-        id: 5,
-        linkUrl:""
-      }
-    ]
-  }
+const Directory = (props) => {
+  return (
+    <div className="directory-menu">
+      {props.sections.map(section => {
+        return <MenuItem key={section.id} title={section.title} image={section.imageUrl} size={section.size} linkUrl={section.linkUrl}/>
+      })}
+    </div>
+  );
+}
 
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(section => {
-          return <MenuItem key={section.id} title={section.title} image={section.imageUrl} size={section.size} linkUrl={section.linkUrl}/>
-        })}
-      </div>
-    );
+const mapStateToProps = (state) => {
+  return {
+    sections: state.directory.sections
   }
 }
 
-export default Directory;
+export default connect(mapStateToProps)(Directory);
