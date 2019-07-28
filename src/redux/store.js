889abac862219352b83ad68ed474022fe5f39a4f
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-28 08:54:05 -0400

    Agregado redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,9 @@ import {createStore, applyMiddleware} from "redux";
 import rootReducer from "./rootReducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 import {persistStore} from "redux-persist";
+import thunk from "redux-thunk";
 
-const middlewares = [];
+const middlewares = [thunk];
 const composedEnhancer = composeWithDevTools(applyMiddleware(...middlewares));
 export const store = createStore(rootReducer, composedEnhancer);
 export const persistor = persistStore(store);
