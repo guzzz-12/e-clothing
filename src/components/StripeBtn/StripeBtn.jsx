a185ff6fac9645eb719bdef95fc1841eedd8889c
J3duardo <jesus_69eduardo@hotmail.com>
2019-07-31 10:28:07 -0400

    Backend: App conectada con el Backend para realizar los pagos con Stripe
    Se creó un backend mediante Express para realizar los pagos de manera segura mediante Stripe Payments y se conectó la aplicación con dicho backend para completar la funcionalidad"

--- src/components/StripeBtn/StripeBtn.jsx
+++ src/components/StripeBtn/StripeBtn.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeBtn = (props) => {
   const priceInCents = props.price * 100;
   const publicApiKey = "pk_test_jBRvI6n6qdWpsRB2FZXRfcVu00USOydagp";
 
   const onToken = (token) => {
-    alert("Payment Successful")
+    axios({
+      url: "payment",
+      method: "post",
+      data: {
+        amount: priceInCents,
+        token: token
+      }
+    })
+    .then((response) => {
+      alert("Payment successful")
+    })
+    .catch((error) => {
+      console.log(`Peyment error: ${JSON.parse(error)}`);
+      alert("There was an issue with your payment. Please make sure you used a correct cedit card")
+    });
   }
 
   return (
