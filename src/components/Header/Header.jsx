8be46d54ed8184c1d4c5d2020bc954cd05a80f04
J3duardo <jesus_69eduardo@hotmail.com>
2019-08-01 09:45:17 -0400

    Header: Corregido logo, se muestra el nombre del usuario logueado

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "./header.scss";
 import {Link} from "react-router-dom";
-import {ReactComponent as HeaderLogo} from "../../assets/crown.svg";
+import crownLogo from "../../assets/crown-logo.png";
 import {auth} from "../../firebase/firebaseUtils";
 import {connect} from "react-redux";
 import CartIcon from "../Cart-Icon/Cart-Icon";
@@ -11,15 +11,16 @@ const Header = (props) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
-        <HeaderLogo className="logo" />
+        <img src={crownLogo} alt="crown logo" className="logo" />
       </Link>
+      {props.currentUser && <div className="userName">Welcome, {props.currentUser.displayName}</div>}
       <div className="options">
         <Link className="option" to="/shop">Shop</Link>
         <Link className="option" to="/contact">Contact</Link>
         {props.currentUser ?
           <div className="option" onClick={() => auth.signOut()}>Sign out</div>
           :
-          <Link className="option" to="/signin">Sign in</Link>
+          <Link className="option" to="/signin">Sign in to start shopping!</Link>
         }
         <CartIcon />
       </div>
